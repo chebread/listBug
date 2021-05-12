1f0da5045477575c6fbd61630aaeba4fa9f9805b
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-05-13 01:55:16 +0900

    version: 1

---
+++ list_to_do_bug_fixes.py
@@ -0,0 +1,11 @@
+# 버그 해결 리스트 프로그램
+print("-- Bug To Do List Program --")
+print("(q:quit)")
+print("-- Input --")
+text = []
+while True:
+    text = input("- ")
+    if text == "q":
+        print("== (QUIT) ==")
+        break
+
